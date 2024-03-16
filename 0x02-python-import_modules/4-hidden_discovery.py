a7c6aa7341fe563df60fd383196a45b003e9cb21
buntunji <scia.ng.za@gmail.com>
2024-03-16 04:07:10 -0700

    print all functions imported from a file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    allfunc = dir()
+    for i in range(0, len(allfunc)):
+        if allfunc[i][:2] != "__":
+            print("{:s}".format(allfunc))
