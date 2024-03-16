ad39a06c393ee2d6860ee7b66811c2d2c442f264
buntunji <scia.ng.za@gmail.com>
2024-03-16 07:59:29 -0700

    my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     num_args = len(argv)
     if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
